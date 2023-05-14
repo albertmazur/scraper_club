752e1ae37fc64d6d8017be139443e4e04bdd0c19
Albert <albert153@interia.pl>
2023-05-14 22:40:06 +0200

    Add projekt

---
+++ main.py
@@ -0,0 +1,128 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getSoup(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    return BeautifulSoup(response.text, 'html.parser')
+domain = "https://www.transfermarkt.pl"
+url = "https://www.transfermarkt.pl/wettbewerbe/europa"
+
+
+soup = getSoup(url)
+table = soup.find('table', {'class': 'items'})
+trs = table.find_all('tr', class_=['odd', 'even'])
+
+plik = open("dane_klubu.txt", "w", encoding="utf-8")
+
+#def getCouch(soup):
+
+    # div = soup.find("div", {"class", "spielername"})
+    # divName = div.find("div", {"class", "staff-name"})
+    # divAge = div.find("div", {"class", "staff-age"})
+
+    # responseCouch = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    # soupCouch = BeautifulSoup(responseCouch.text, 'html.parser')
+
+    # div = soup.find("div", {"class", "staff-slider-headline"})
+
+    # print(divName.text+" | "+divAge.text)
+
+
+def getTrophist(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, verify=False)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    divs = soup.find_all("div", {"class": "erfolg_bild_box"})
+    for div in divs:
+        mainDiv = div.parent
+        divName = mainDiv.find("div", {"class", "erfolg_bild_box"})
+        img = divName.find("img")
+
+        divSeson = mainDiv.find("div", {"class", "erfolg_infotext_box"})
+        sesonText = ''.join(divSeson.text.split())
+        sesonText = sesonText.replace(",", ", ")
+        #print(img.get("title")+" | "+sesonText)
+        plik.write(img.get("title")+" | "+sesonText+"\n")
+
+
+def getPlayers(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    #getCouch(soup)
+
+    table = soup.find('table', {'class': 'items'})
+    trs = table.find_all('tr', class_=['odd', 'even'])
+
+    for i, tr in enumerate(trs):
+        # Imię, nazwisko i pozycja
+        table = tr.find("table", {'class': 'inline-table'})
+        trName = table.find_all("tr")
+        tdName = trName[0].find("td", {'class': 'hauptlink'})
+        name = tdName.find("a").getText()
+        position = trName[1].find("td").getText()
+
+        tds = tr.find_all("td", {'class': 'zentriert'})
+        # Data urodzenia
+        dateBirth = tds[1].text[:-5]
+        # Narodowość
+        nation = tds[2].find("img").get("title")
+        #nation = ""
+
+        # Wartość rynkowa
+        tdWalue = tr.find("td", {'class': 'rechts hauptlink'})
+        a = tdWalue.find("a")
+
+        if a is not None:
+            walue = a.text
+        else:
+            walue = "0 €"
+
+        #print(str(i+1)+" | "+name+" | "+position+" | "+dateBirth+" | "+nation+" | "+walue)
+        plik.write(str(i+1)+" | "+name+" | "+position+" | "+dateBirth+" | "+nation+" | "+walue+"\n")
+
+    tropyLink = soup.find("div", {'class', 'data-header__badge-container'})
+    tropyLink = tropyLink.find("a")
+    if tropyLink is not None:
+        url = tropyLink.get('href')
+        getTrophist(domain + url)
+    #print(domain+url)
+
+    div = soup.find_all("div")
+    print(div)
+
+def getClubs(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    table = soup.find('table', {'class': 'items'})
+    trs = table.find_all('tr', class_=['odd', 'even'])
+
+    for i, tr in enumerate(trs):
+        td = tr.find("td", {'class': 'hauptlink'})
+        a = td.find("a")
+
+        #print(str(i+1)+" | "+a.get("title")+" | "+domain+a.get("href"))
+        plik.write(str(i+1)+" | "+a.get("title")+" | "+domain+a.get("href")+"\n")
+
+        getPlayers(domain+a.get("href"))
+
+
+for i, tr in enumerate(trs):
+    table = tr.find("table", {'class': 'inline-table'})
+    tds = table.find_all("td")
+    td = tds[1]
+    a = td.find("a")
+
+    td2 = tr.find_all("td", {'class': 'zentriert'})
+    img = td2[0].find("img")
+
+    countClub = td2[1].text
+
+    #print(str(i+1)+" | "+a.get("title")+" | "+img.get("title")+" | "+countClub+" | "+domain + a.get('href'))
+    plik.write(str(i+1)+" | "+a.get("title")+" | "+img.get("title")+" | "+countClub+" | "+domain + a.get('href')+"\n")
+
+    getClubs(domain + a.get('href'))
+
+
+plik.close()
